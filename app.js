13aab48e9b86df8966776bc56677ab20f9d5ed01
Tomas Padilla <padillatomasagustin@gmail.com>
2020-12-15 07:53:38 +0100

    Comenzamos Cookies

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.set('views', 'views');
 // Routes:
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -71,6 +72,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
